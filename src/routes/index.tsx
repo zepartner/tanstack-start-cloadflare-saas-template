93d9243a03b7c7c20fd615ba9ae35610f92749bc
Gregory - ZE Partner <help@zepartner.net>
2026-01-03 13:26:19 +0000

    Enhance home page layout with centered title and dashboard link button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
 /** @format */
 
-import { createFileRoute } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
+import { createFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({ component: App });
 
 function App() {
 	return (
 		<div className="min-h-screen">
-			<h1>Our App Home Page</h1>
+			<div className="w-full flex justify-center gap-8 flex-col p-10">
+				<h1 className="text-2xl font-bold text-center">Our App Home Page</h1>
+				<Button asChild>
+					<Link to="/dashboard">Go to Dashboard</Link>
+				</Button>
+			</div>
 		</div>
 	);
 }
